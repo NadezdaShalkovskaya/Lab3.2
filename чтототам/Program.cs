ba215e6a845e13928a1e4bf201e4cf3e30f3d1a7
Надежда Шальковская <Shalk000@kbkit.pri>
2024-11-18 11:46:55 +0200

    first commit

---
+++ чтототам/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApplicationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A_1 = 1;
+            double A_2 = 2;
+            double A_3 = (A_1+A_2)/2;
+            Console.WriteLine("D=");
+            double D = double.Parse(Console.ReadLine());
+            int k = 3;
+            while (Math.Abs(A_3*A_2)>=D)
+            {
+                A_1 = A_2;
+                A_2 = A_3;
+                A_3= (A_1+A_2)/2;
+                k++;
+            }
+            Console.WriteLine("k = {0}; Ak-1={1};Ak={2};k,A_2,A_3");
+            Console.ReadLine();
+        }
+    }
+}
